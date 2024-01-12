65f8b21a0c3ce96ae12988dc0a6da431faa6de87
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-12 17:01:14 +0800

    added readjson

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
+
 function main() {
   console.log("hwllo rold");
 }
 
 function saveJson() {}
+
+function readJson() {
+  try {
+    const jsonData = fs.readFileSync("/data/saveData.json", "utf-8");
+    const data = JSON.parse(jsonData);
+
+    return data;
+  } catch (error) {
+    console.log("File doesnt exists, creating a new one.");
+
+    //todo SAVE HERE
+  }
+}
 
 main();
