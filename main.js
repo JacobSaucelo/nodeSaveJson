5dc0190bdabab334ec42b4b6e1153aaee74a4c5f
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-12 17:24:30 +0800

    working code base

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const fs = require("fs");
 
 function main() {
-  console.log("hwllo rold");
+  const saveData = readJson();
+  console.log("saveData: ", saveData);
 }
 
 function readJson() {
   try {
-    const jsonData = fs.readFileSync("/data/saveData.json", "utf-8");
+    const jsonData = fs.readFileSync("./data/saveData.json", "utf-8");
     const data = JSON.parse(jsonData);
 
     return data;
   } catch (error) {
     console.log("File doesnt exists, creating a new one.");
 
+    const initialSave = {};
+
+    saveJson(initialSave);
+    return initialSave;
     //todo SAVE HERE
   }
 }
@@ -20,7 +25,7 @@ function readJson() {
 function saveJson(data) {
   try {
     const jsonData = JSON.stringify(data, null, 4);
-    fs.writeFileSync("/data/saveData.json");
+    fs.writeFileSync("./data/saveData.json", jsonData);
 
     console.log("Data Saved.");
   } catch (error) {
